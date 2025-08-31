84b39ae9d025345d4e307c33214aa8afcb39a6bc
Luke <luke@onlyfastcode.com>
2025-08-31 22:41:22 +0200

    even more minimalist labwc

--- break-reminder/.local/bin/break-reminder.sh
+++ break-reminder/.local/bin/break-reminder.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 while true; do
-  for i in {1..30}; do
-    clear
-    fastfetch
-     echo "$((30 - i)) minutes to break"
-    sleep 60  # sleep for 1 minute
-  done
+  sleep 1800
   notify-send -u normal -t 10000 "Break Reminder" "Time to take a screen break!"
 done
 
