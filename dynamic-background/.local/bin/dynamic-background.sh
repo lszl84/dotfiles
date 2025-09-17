6810cab1f37d6268c3139665d9495d0dd3f894cc
luke <luke@onlyfastcode.com>
2025-09-17 09:00:02 +0200

    another background idea - text on top left

--- dynamic-background/.local/bin/dynamic-background.sh
+++ dynamic-background/.local/bin/dynamic-background.sh
@@ -5,10 +5,11 @@ my_bck="$HOME/.config/labwc/ape.jpg"
 
 pidof swaybg && killall swaybg
 
-# run first to avoid imagemagick delay
-swaybg -m fit -i "$my_bck" &
-my_swaypid=$!
-my_swaypid_new=0
+swaybg -m fill -i "$my_bck" &
+my_swaypid1=0
+my_swaypid_new1=0
+my_swaypid2=0
+my_swaypid_new2=0
 
 while true; do
   # 90 iterations of 20s = 30min. TODO: use variables
@@ -18,23 +19,47 @@ while true; do
     echo >> /tmp/ff.txt
     date '+%A, %d %B %Y %H:%M' >> /tmp/ff.txt
     echo >> /tmp/ff.txt
-    #[ "$(apk version -l '<' | wc -l)" -gt 1 ] && echo "Updates are available!" >> /tmp/ff.txt
  
     checkupdates --nocolor >> /tmp/ff.txt
 
-    magick "$my_bck" -font "DejaVu-Sans-Mono" -pointsize 56 -fill '#999999' \
--gravity northwest -annotate +80+80 "$(cat /tmp/ff.txt)" /tmp/background.png
-    #magick -background transparent -font "DejaVu-Sans-Mono" -pointsize 32 -fill '#999999' label:"$(cat /tmp/ff.txt)" /tmp/background.png
-    swaybg -m fit -i /tmp/background.png &
-    my_swaypid_new=$!
+    # magick -size 3120x2080 canvas:transparent \
+    #    -font "DejaVu-Sans-Mono" -pointsize 28 -fill '#999999' \
+    #    -annotate +80+100 "$(cat /tmp/ff.txt)" \
+    #    /tmp/background_3k.png
+    
+    # magick -size 5120x2880 canvas:transparent \
+    #    -font "DejaVu-Sans-Mono" -pointsize 28 -fill '#999999' \
+    #    -annotate +80+100 "$(cat /tmp/ff.txt)" \
+    #    /tmp/background_5k.png
 
+    magick "$my_bck" -resize 3120x2080^ -gravity center -extent 3120x2080 \
+       -gravity none \
+       -font "DejaVu-Sans-Mono" -pointsize 28 -fill '#999999' \
+       -annotate +80+100 "$(cat /tmp/ff.txt)" \
+       /tmp/background_3k.png
+
+    magick "$my_bck" -resize 5120x2880^ -gravity center -extent 5120x2880 \
+       -gravity none \
+       -font "DejaVu-Sans-Mono" -pointsize 28 -fill '#999999' \
+       -annotate +80+100 "$(cat /tmp/ff.txt)" \
+       /tmp/background_5k.png
+    swaybg -o DP-2 -i /tmp/background_5k.png -m fill &
+    my_swaypid_new1=$!
+    
+    swaybg -o eDP-1 -i /tmp/background_3k.png -m fill &
+    my_swaypid_new2=$!
+    
     # avoid swaybg flicker after kill
-    sleep 1 
-    [ "$my_swaypid" -gt 0 ] && kill $my_swaypid
+    sleep 1
+    [ "$my_swaypid1" -gt 0 ] && kill $my_swaypid1
+    [ "$my_swaypid2" -gt 0 ] && kill $my_swaypid2
 
     # total 20 seconds between refreshes
     sleep 19
-    my_swaypid=$my_swaypid_new
+    my_swaypid1=$my_swaypid_new1
+    my_swaypid2=$my_swaypid_new2
+
+    ps aux |grep swaybg
   done
   notify-send -u normal -t 10000 "Break Reminder" "Time to take a screen break!"
 done
