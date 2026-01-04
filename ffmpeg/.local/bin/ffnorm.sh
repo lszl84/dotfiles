6c7271e82fcc1d47c576cd05b607b55c393ed0de
luke <luke@devmindscape.com>
2026-01-04 20:56:25 +0100

    better ffmpeg chain for the Neumann mic

--- ffmpeg/.local/bin/ffnorm.sh
+++ ffmpeg/.local/bin/ffnorm.sh
@@ -8,13 +8,14 @@ fi
 INPUT="$1"
 OUTPUT="${2:-processed_$1}"
 
-ffmpeg -hide_banner -loglevel quiet -i "$INPUT" -af \
-"arnndn=m=$HOME/dotfiles/denoise/sh.rnnn,\
-acompressor=threshold=0.05:ratio=2:attack=10:release=100,\
-volume=21dB,\
-equalizer=f=700:width_type=h:width=150:g=-4,\
-equalizer=f=15000:width_type=h:width=2000:g=2" \
-"$OUTPUT" 
+ffmpeg -hide_banner  -i "$INPUT" -af \
+       "volume=21dB,\
+alimiter=limit=-1dB:attack=5:release=50,\
+highpass=f=100,\
+equalizer=f=700:width_type=q:width=0.7:gain=-6,\
+highshelf=f=6000:g=6,
+volume=8dB" \
+"$OUTPUT"
 
 echo "--- $INPUT:"
 
