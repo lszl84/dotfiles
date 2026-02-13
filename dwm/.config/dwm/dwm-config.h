0d639b7977fd09a9bac7e0ce9f5261baa066d320
luke <luke@devmindscape.com>
2026-02-13 18:08:01 +0000

    dwm updates

--- dwm/.config/dwm/dwm-config.h
+++ dwm/.config/dwm/dwm-config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows (2x for HiDPI) */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 14;       /* gap pixel between windows (7 logical @ HiDPI) */
 static const int showbar            = 1;        /* 0 means no bar */
